da77d875975bfd936e0080c4e8309bcfbf77e89b
abhishek08112000 <abhisheknayak.xworkz@gmail.comgit config --global user.name abhishek08112000git config --global user.email abhisheknayak.xworkz@gmail.com>
2022-09-16 16:58:03 +0530

    commiting java files

---
+++ Abhi.java
@@ -0,0 +1,30 @@
+class Abhi{
+
+	public static void main(String m[]){
+	
+	
+	int a = 5;
+	int b = 15;
+	int c = 20;
+	
+	int r = (a*b*c);
+	
+	int result = a+b+c ;
+	
+	
+	System.out.println("Multiplication of a b c is "+ r);
+	System.out.println("*******************");
+	
+	System.out.println("Addition of a b c is "+ result);
+		
+	
+	
+	
+	
+	
+	}
+
+
+
+
+}
